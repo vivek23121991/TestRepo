d406b74635ef11ed6e8c31d0c600c95658a677f1
vivek23121991 <vnvicky90@gmail.com>
2025-09-06 16:14:33 +0530

    Added java file

---
+++ src/test/java/javaPrograms/CountOfChsr.java
@@ -0,0 +1,13 @@
+package javaPrograms;
+
+public class CountOfChsr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name= "Viaan Vivek Mokina Nikalje";
+		name = name.replaceAll(" ", "").toLowerCase();
+
+	}
+
+}
