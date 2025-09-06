a8f01406db3564fd7d2fc0f4c42fe6d0d45e5744
vivek23121991 <vnvicky90@gmail.com>
2025-09-06 16:21:36 +0530

    count of char java program

--- src/test/java/javaPrograms/CountOfChsr.java
+++ src/test/java/javaPrograms/CountOfChsr.java
@@ -1,5 +1,8 @@
 package javaPrograms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountOfChsr {
 
 	public static void main(String[] args) {
@@ -7,6 +10,22 @@ public class CountOfChsr {
 		
 		String name= "Viaan Vivek Mokina Nikalje";
 		name = name.replaceAll(" ", "").toLowerCase();
+		
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		
+		//read each character and store the count in map
+		
+		for(char ch :name.toCharArray())
+		{
+			map.put(ch, map.getOrDefault(ch, 0)+1);
+		}
+		
+		// print the count using map
+		
+		for(Map.Entry entry: map.entrySet())
+		{
+			System.out.println(entry.getKey()+":"+entry.getValue());
+		}
 
 	}
 
